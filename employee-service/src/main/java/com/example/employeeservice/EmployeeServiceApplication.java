a8d1e509531f0ad60c9ceb5f572429e699be4544
mukesh <saimukesh944@gmail.com>
2023-11-27 01:01:57 +0530

    new commit

---
+++ employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
@@ -0,0 +1,25 @@
+package com.example.employeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class EmployeeServiceApplication {
+
+//	@Bean
+//	public RestTemplate restTemplate(){
+//		return new RestTemplate();
+//	}
+
+//	@Bean
+//	public WebClient webClient(){
+//		return WebClient.builder().build();
+//	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+}
---
+++ employee-service/src/main/java/com/example/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,79 @@
+package com.example.employeeservice.service.impl;
+
+import com.example.employeeservice.dto.APIResponseDto;
+import com.example.employeeservice.dto.DepartmentDto;
+import com.example.employeeservice.dto.EmployeeDto;
+import com.example.employeeservice.entity.Employee;
+import com.example.employeeservice.repository.EmployeeRepository;
+import com.example.employeeservice.service.APIClient;
+import com.example.employeeservice.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+   // private RestTemplate restTemplate;
+   // private WebClient webClient;
+    private APIClient apiClient;
+
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail(),
+                employeeDto.getDepartmentCode()
+        );
+
+        Employee saveDEmployee = employeeRepository.save(employee);
+
+        EmployeeDto savedEmployeeDto = new EmployeeDto(
+                saveDEmployee.getId(),
+                saveDEmployee.getFirstName(),
+                saveDEmployee.getLastName(),
+                saveDEmployee.getEmail(),
+                saveDEmployee.getDepartmentCode()
+        );
+
+        return savedEmployeeDto;
+    }
+
+    @Override
+    public APIResponseDto getEmployeeById(Long employeeId) {
+
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
+
+//        DepartmentDto departmentDto = webClient.get()
+//                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+//                .retrieve()
+//                .bodyToMono(DepartmentDto.class)
+//                .block();
+
+        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+
+        EmployeeDto employeeDto = new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getDepartmentCode()
+        );
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+
+        return apiResponseDto;
+    }
+}
