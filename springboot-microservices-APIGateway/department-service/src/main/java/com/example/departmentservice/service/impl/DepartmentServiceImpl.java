e0dd6d8e319c9966fe266642f1b30a2c7fd7675d
mukesh <saimukesh944@gmail.com>
2023-11-27 20:18:33 +0530

    api commit

---
+++ springboot-microservices-APIGateway/department-service/src/main/java/com/example/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.departmentservice.service.impl;
+
+import com.example.departmentservice.entity.Department;
+import lombok.AllArgsConstructor;
+import com.example.departmentservice.dto.DepartmentDto;
+import com.example.departmentservice.repository.DepartmentRepository;
+import com.example.departmentservice.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+
+        // convert department dto to department jpa entity
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = new DepartmentDto(
+                savedDepartment.getId(),
+                savedDepartment.getDepartmentName(),
+                savedDepartment.getDepartmentDescription(),
+                savedDepartment.getDepartmentCode()
+        );
+
+        return savedDepartmentDto;
+    }
+
+    @Override
+    public DepartmentDto getDepartmentByCode(String departmentCode) {
+
+        Department department = departmentRepository.findByDepartmentCode(departmentCode);
+
+        DepartmentDto departmentDto = new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription(),
+                department.getDepartmentCode()
+        );
+        return departmentDto;
+    }
+}
