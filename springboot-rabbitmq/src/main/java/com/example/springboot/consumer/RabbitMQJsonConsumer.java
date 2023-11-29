f05313ac7351517bc9cd0f1edab7a3d938159e32
mukesh <saimukesh944@gmail.com>
2023-11-29 23:33:41 +0530

    commit rabbit

---
+++ springboot-rabbitmq/src/main/java/com/example/springboot/consumer/RabbitMQJsonConsumer.java
@@ -0,0 +1,18 @@
+//package com.example.springboot.consumer;
+//
+//import com.example.springboot.dto.User;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class RabbitMQJsonConsumer {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
+//
+//    @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
+//    public void consumeJsonMessage(User user){
+//        LOGGER.info(String.format("Received JSON message -> %s", user.toString()));
+//    }
+//}
---
+++ springboot-rabbitmq/src/main/java/com/example/springboot/controller/MessageJsonController.java
@@ -0,0 +1,26 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.dto.User;
+import com.example.springboot.publisher.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer) {
+        this.jsonProducer = jsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        jsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sent to RabbitMQ ...");
+    }
+}
